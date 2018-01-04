b20d4811b2abbeb110778a93bbcd74f236986365
KYLE LIPTON <lipton.kyle@gmail.com>
2018-01-03 21:41:28 -0500

    api setup

--- election.js
+++ election.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   // Imagination!
+  $.ajax({
 
+    url: "https://bb-election-api.herokuapp.com/",
+    method: "GET",
+    dataType: "JSON",
+  }).done(function(responseData){
+    var ul = document.querySelector('#candidates');
+
+    data.candidates.forEach(function(candidate){
+      li = document.createElement("li");
+      li.innerHTML = '<b>' + candidate.name + ':</b>' + candidate.votes + ' votes';
+      ul.appendChild(li);
+
+    });
+  });
 });
